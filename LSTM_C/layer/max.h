ecbb9b32ee764e227dc7c68e04b26936e25f1fb6
aya03sora@gmail.com <a03ki04@gmail.com>
2018-11-13 14:43:53 +0900

    ヘッダファイルに分けた

---
+++ LSTM_C/layer/max.h
@@ -0,0 +1,22 @@
+#ifndef MAX_H
+#define MAX_H
+
+/*
+max : 配列の最大値のインデックスを返す関数
+
+Parameters
+----------
+input_x : array of pointer to float
+  一次元配列
+len : int
+  一次元配列の要素数
+
+Return
+------
+max_index : int
+  最大値のインデックス
+*/
+
+int max(float *, int);
+
+#endif
