1b6ca20f4f3bbaacc8fca87de53c7a80080b2705
aya03sora@gmail.com <a03ki04@gmail.com>
2018-12-12 21:29:38 +0900

    @detailの追加

--- LSTM_C/function/relu.c
+++ LSTM_C/function/relu.c
@@ -4,12 +4,13 @@
 * @param[out] input_x 一次元配列
 * @param[in] len 一次元配列の要素数
 * @return void
+* @detail 入力値input_xを書き換えてしまいます
 */
 
 #include <stdio.h>
 #include "relu.h"
 
-void relu(float *input_x, int len){
+void relu(float *input_x,　int len){
   int i;
   for(i = 0; i < len; i++){
     if(input_x[i] < 0){
