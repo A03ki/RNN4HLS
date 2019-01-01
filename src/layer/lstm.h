a90988959a69bd737fd07f2fae71600cabd3fe9a
Aki <a03ki04@gmail.com>
2019-01-01 13:56:10 +0900

    [Update] arguments of lstm 'add weight, bias, ...etc'
    <Because> lstm layer is used many times in a model

--- src/layer/lstm.h
+++ src/layer/lstm.h
@@ -10,8 +10,18 @@ output : array of pointer to float
   一次元配列(出力用)
 input_x : const array of pointer to float
   一次元配列(入力用)
+weight_x : const array of pointer to float
+  入力input_xと行列積を行う一次元配列の重み
+weight_h : const array of pointer to float
+  時刻t-1の時の出力outputと行列積を行う一次元配列の重み
+bias : const array of pointer to float
+  一次元配列のバイアス. 転置した重みの行と同じ長さを持つ
 row : int
   input_xが二次元配列のときの列の長さ
+matrix_k : int
+  input_xが二次元配列のときの行の長さ(weight_hが二次元配列のときの列の長さ)
+column : int
+  weight_hが二次元配列のときの行の長さ
 
 Return
 ------
@@ -24,6 +34,6 @@ Pytorchの重み(weight_ih_l0, weight_hh_l0)は転置して使う
 Pytorchのバイアス(bias_ih_l0, bias_hh_l0)は足して使う
 */
 
-void lstm(float *, const float *, int);
+void lstm(float *, const float *, const float *, const float *, const float *, int, int, int);
 
 #endif
