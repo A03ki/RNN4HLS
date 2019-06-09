3be60c134ff9538e1e01befb91ba960f58b5f527
Aki <a03ki04@gmail.com>
2019-06-09 14:07:18 +0900

    [Fix] this file to make the directory `build`
    <Because> an error occurs when there is not this directory

--- test/make.py
+++ test/make.py
@@ -1,7 +1,10 @@
+import os
 import subprocess
 
 
 def run_make():  # build .so file by make command
+    if not os.path.exists('build'):
+        os.mkdir('build')
     proc = subprocess.run(["make"], stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE)
     return proc.stdout.decode("utf8"), proc.stderr.decode("utf8")
